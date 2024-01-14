2e649d17f37196fe61143608797fd737aaa71e34
Damirdine ALI SOILIHI <damirdine@outlook.fr>
2024-01-14 14:55:43 +0100

    card component

--- src/challenges/qr-code-component-main/qrCode.tsx
+++ src/challenges/qr-code-component-main/qrCode.tsx
@@ -1,13 +1,25 @@
 import qrCodeImg from "./images/image-qr-code.png";
 
-function QrCodePage() {
+export default function QrCodePage(
+  onClick = () => {
+    alert("onclick");
+  }
+) {
+  const { img, title, description } = {
+    img: qrCodeImg,
+    title: "Improve your front-end skills by building projects",
+    description:
+      "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level",
+  };
   return (
     <>
-      <main className="min-h-screen bg-gray-400">
-        <img src={qrCodeImg} alt="" />
+      <main className="min-h-screen bg-gray-400 flex items-center justify-center">
+        <article className="bg-white grid w-full sm:w-4/5 md:w-3/4 lg:w-1/2 text-center p-3 rounded-2xl gap-4 max-w-80 lg:w-9">
+          <img src={img} alt="" className="rounded-lg" onClick={onClick} />
+          <h3 className="text-2xl font-bold">{title}</h3>
+          <p className="text-gray-600 mb-5">{description}</p>
+        </article>
       </main>
     </>
   );
 }
-
-export default QrCodePage;
--- src/pages/_index.tsx
+++ src/pages/_index.tsx
@@ -1,9 +1,17 @@
 export default function Home() {
+  const challenges = [{ name: "qrcode component", url: "/qr-code" }];
   return (
     <>
-      <p className="bg-red-100 m-5 p-5 text-red-600 font-bold text-3xl">
-        Hello
-      </p>
+      {challenges.map((el) => {
+        return (
+          <a
+            href={el.url}
+            // className="bg-red-100 mx-5 mt-28 p-5 text-red-600 font-bold text-3xl"
+          >
+            {el.name}
+          </a>
+        );
+      })}
     </>
   );
 }
